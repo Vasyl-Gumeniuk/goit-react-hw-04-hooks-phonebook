10f945883caaad818039bd28778c3239c6bf3c5f
Vasyl-Gumeniuk <vasyl.gumeniuk@gmail.com>
2022-08-08 14:10:23 +0300

    fixed contacts list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,13 +15,6 @@ Notiflix.Notify.init({
 });
 
 export const App = () => {
-  // const [contacts, setContacts] = useState([
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ]);
-
   const [filter, setFilter] = useState('');
   const [contacts, setContacts] = useLocalStorage(LOCALSTORAGE_KEY, []);
 
